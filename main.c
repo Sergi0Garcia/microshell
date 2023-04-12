0870ae9c6a91adbc70616856903d3299e74f93d2
Sergio Garcia <segarcia@student.42heilbronn.de>
2023-04-12 15:22:07 +0200

    ckpt

--- main.c
+++ main.c
@@ -5,80 +5,89 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: segarcia <segarcia@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/11 12:28:39 by segarcia          #+#    #+#             */
-/*   Updated: 2023/04/12 11:12:22 by segarcia         ###   ########.fr       */
+/*   Created: 2023/04/12 14:48:59 by segarcia          #+#    #+#             */
+/*   Updated: 2023/04/12 15:21:46 by segarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
-#include <stdio.h>
 
 int g_status = 0;
 
-int ft_error(char *str, char *arg)
+void ft_error(char *str, int newline)
 {
-	while (*str)
-		write(2, str++, 1);
-	if (arg)
-		while(*arg)
-			write(2, arg++, 1);
-	write(2, "\n", 1);
-	return (1);
-}`
+    int i;
 
-int ft_execute(char **argv, int i, int tmp_fd, char **env)
+    i = 0;
+    while (str[i])
+    {
+        write(2, &str[i], 1);
+        i++;
+    }
+    if (newline)
+        write(2, "\n", 1);
+}
+
+int ft_execute(char **argv, char **env, int i, int fd)
 {
-    int status;
-
-    status = 0;
     argv[i] = NULL;
-    dup2(tmp_fd, 0);
-    close(tmp_fd);
-    status = execve(argv[0], argv, env);
-    g_status = status;
-    return (status);
+    dup2(fd, STDIN_FILENO);
+    close(fd);
+    g_status = execve(argv[0], argv, env);
+    return (g_status);
 }
 
 int main(int argc, char **argv, char **env)
 {
-    int i;
+    int i = 0;
     int fd[2];
-    int tmp_fd;
-    (void) argc;
+    int tmp_fd = dup(STDIN_FILENO);
+    g_status = 0;
 
-    i = 0;
-    tmp_fd = dup(0);
+    (void) argc;
+    (void) env;
+    (void) tmp_fd;
+    (void) fd;
 
     while (argv[i] && argv[i + 1])
     {
         argv = &argv[i + 1];
-        i = 0;
+        i  = 0;
         while (argv[i] && strcmp(argv[i], ";") != 0 && strcmp(argv[i], "|") != 0)
             i++;
         if (strcmp(argv[0], "cd") == 0)
         {
             if (i != 2)
-                ft_error("error: cd: bad arguments", NULL);
-            else if (chdir(argv[1]) != 0)
+                ft_error("error: cd: bad arguments", 1);
+            else
             {
                 g_status = chdir(argv[1]);
-                ft_error("error: cd: cannot change directory to ", argv[1]);
-                return (g_status);
+                if (g_status != 0)
+                {
+                    ft_error("error: cd: cannot change directory to ", 0);
+                    ft_error(argv[1], 1);
+                    return (g_status);
+                }
             }
         }
         else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0))
         {
             if (fork() == 0)
             {
-                if (ft_execute(argv, i, tmp_fd, env) != 0)
+                g_status = ft_execute(argv, env, i, tmp_fd);
+                if (g_status != 0)
+                {
+                    ft_error("error: cannot execute: ", 0);
+                    ft_error(argv[0], 1);
                     return (g_status);
+                }
             }
             else
             {
                 close(tmp_fd);
-                while(waitpid(-1, NULL, WUNTRACED) != -1);
+                while (waitpid(-1, &g_status, 2) != -1);
                 tmp_fd = dup(0);
             }
         }
@@ -90,8 +99,13 @@ int main(int argc, char **argv, char **env)
                 dup2(fd[1], 1);
                 close(fd[0]);
                 close(fd[1]);
-                if (ft_execute(argv, i, tmp_fd, env) != 0)
+                g_status = ft_execute(argv, env, i, tmp_fd);
+                if (g_status != 0)
+                {
+                    ft_error("error: cannot execute: ", 0);
+                    ft_error(argv[0], 1);
                     return (g_status);
+                }
             }
             else
             {
@@ -101,6 +115,6 @@ int main(int argc, char **argv, char **env)
             }
         }
     }
-    close(tmp_fd);
+    close (tmp_fd);
     return (g_status);
 }
